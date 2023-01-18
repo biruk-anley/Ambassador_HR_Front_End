b2ecec30cf3c4e67f9dada2bd3c93b0a9b23fe1f
biruk_anley <birukanley2022@gmail.com>
2023-01-18 09:52:28 +0300

    add progress sheet

---
+++ src/progress.js
@@ -0,0 +1,5 @@
+// completed footer componenet with ambassador icon ...the icon can be used in different area
+
+//tasks
+//1 removing un neccessary thing and make it simple and easy
+//2 if so finsih the notice board asap
