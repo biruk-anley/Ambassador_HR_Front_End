7e5bae9b609a33793b1f9bbe670397612e7b30fb
Biruk-Tassew <biruktassew21@gmail.com>
2023-02-12 16:00:38 -0500

    refactor(frontend): refactor routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import { Container, withStyles, makeStyles } from "@material-ui/core";
-import { useDispatch, useSelector } from "react-redux";
+import { Container, makeStyles } from "@material-ui/core";
 import NavTabs from "./components/NavTabs";
-
-import AddHouse from './pages/AddEmployee/AddEmployee'
-
-/*import LoginRegister from './pages/Authentication/LoginRegister'*/
-//comment
 import LoginRegister from "./pages/Authentication/Login";
 import NoticeBoard from "./pages/NoticeBoard";
-import RegisterHouse from "./pages/AddEmployee/AddEmployee";
-
-
-
+import AddEmployee from './pages/AddEmployee/AddEmployee'
 import UpperFooter from "./components/UpperFooter";
 import Employee from "./pages/Employees";
 import readMore from "./pages/DetailNotice";
 import Vote from "./pages/Vote";
-
-
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,29 +20,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function App() {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const [state, setState] = useState({
-    token: "",
-    searchKeyword: "",
-    isAdmin: false,
-  });
-
+  
   const [sideBar, setSideBar] = useState(false);
   const classes = useStyles();
-
- 
-
-  const isAdmin = () => {
-    const tokenString = localStorage.getItem("admin");
-    const user = tokenString ? JSON.parse(tokenString) : false;
-    if (user) {
-      return user.isAdmin;
-    } else {
-      return false;
-    }
-  };
-
   return (
     <div className={classes.root}>
       
@@ -63,50 +30,19 @@ function App() {
         <NavTabs
           // getToken={this.getToken}
           sideBar={sideBar}
-          isAdmin={isAdmin}
         />
         <Container className={classes.mainParts} maxWidth={"xl"}>
           <Route path="/" exact>
             {" "}
             <LoginRegister
               setSideBar={setSideBar}
-              // setToken={setToken}
-              // getToken={getToken}
             />
-          </Route>
-          
-         
-        
-          
-          {/* <Route path="/login" exact>
-            {" "}
-            <LoginRegister
-              setSideBar={setSideBar}
-              // setToken={setToken}
-              // getToken={getToken}
-            />
-          </Route> */}
-
-
-
-       
-          
-          
-        
+          </Route>        
           <Route path="/Noticeboard" component={NoticeBoard} />
           <Route path="/ReadMore" component={readMore} />
           <Route path="/Vote" component={Vote}/>
-
           <Route path="/Employee" component={Employee} />
-          <Route path="/addhouse">
-            {" "}
-            <RegisterHouse
-              setSideBar={setSideBar}
-              // getToken={getToken}
-            />
-          </Route>
-         
-          <Route path="/addEmployee" component={AddHouse} />
+          <Route path="/addEmployee" component={AddEmployee} />
         </Container>
       </BrowserRouter>
       <UpperFooter></UpperFooter>
