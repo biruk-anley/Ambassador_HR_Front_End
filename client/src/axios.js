08f445865daabc010602e53bfce78fd03bb83c7e
Biruk-Tassew <biruktassew21@gmail.com>
2023-02-13 15:41:17 -0500

    fix(frontend): solved the network error

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
-const instance = axios.create({
-    baseURL: "http://localhost:6000"
-})
+const instance = axios.create(
+    {
+            baseURL: "http://localhost:5000/",
+            withCredentials: false,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                
+                "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS",
+                
+                "Access-Control-Allow-Headers": "*"
+                
+                
+          }
+      })
 
 export default instance;
