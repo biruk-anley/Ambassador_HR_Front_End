33b0d8080acb7a071054431220f53ec4b13ece3f
Biruk Anley <birukanley2022@gmail.com>
2023-02-18 22:40:04 +0300

    update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
-const instance = axios.create({
-    baseURL: "http://localhost:6000"
-})
 
+
+const instance = axios.create(
+    {
+        baseURL: "",
+            withCredentials: false,
+            headers: {
+              'Access-Control-Allow-Origin' : '*',
+              'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',   
+          }
+      })
+
+      
 export default instance;
