c51119cf0584ea257cb88ac4f4e3501b95fcfeb2
Biruk Anley <birukanley2022@gmail.com>
2023-03-08 21:16:51 +0300

    update store.js

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import noticesReducer from "../slices/noticesSlice";
 import employeesReducer from "../slices/employeeSlice";
+import evaluationReducer from '../slices/evaluationSlice';
 
 const store = configureStore({
     reducer: {
         notices: noticesReducer,
-        employees: employeesReducer
+        employees: employeesReducer,
+        evaluation: evaluationReducer,
     }
 })
 
